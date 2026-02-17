3d9365ef0a712e50d30d14ec5c5e7a0be2d17409
GeorgeJuarez <125137215+GeorgeJuarez660@users.noreply.github.com>
2026-02-17 22:06:19 +0100

    Delete static directory

--- static/js/move_RL.js
+++
@@ -1,9 +0,0 @@
-function move_right(){
-    //Move the viewing window to the right by 500 px
-    document.documentElement.scrollLeft += 1712;
-}
-
-function move_left(){
-    //Move the viewing window to the right by 500 px
-    document.documentElement.scrollLeft += 0;
-}
